d5d98b1600c4fb63ed2dd8dfa13d82edc162a016
PlusPig <2504722047@qq.com>
2025-09-27 17:06:36 +0800

    for js submit1

---
+++ workjs/hello.js
@@ -0,0 +1,6 @@
+var hello = 20.22
+var name = "strname"
+var char_a = 'a'
+console.log(char_a)
+alert("我是弹出")
+document.writeln("shuchu dao jiemian")
---
+++ workjs/str.js
@@ -0,0 +1,8 @@
+var str1 = "ppqq";
+var str2 = "aaaa";
+document.writeln(str1);
+console.log(str1.charAt(3));
+console.log(str1.concat(str2));
+console.log(str1);
+var str3 = str1 + str2;
+document.writeln("<br>"+str3);
